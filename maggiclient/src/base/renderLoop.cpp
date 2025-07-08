8d49fc182cec553a1bdbbb07764965e08064eb26
Not AG7 <agg3760@gmail.com>
2025-07-08 11:13:50 +0200

    Update watermark design

--- maggiclient/src/base/renderLoop.cpp
+++ maggiclient/src/base/renderLoop.cpp
@@ -10,21 +10,28 @@
 void Base::RenderLoop() // Runs every frame
 {
 	if (!Base::Running) return;
-	const char* watermark = "by ag7-dev.de";
-	// goofy ahh css
-	float margin = 3;
-	float font_size = 24;
+        const char* watermark = "Made by Maggi";
 
-	ImVec2 screenSize = ImGui::GetWindowSize();
-	ImVec2 textSize = Menu::Font->CalcTextSizeA(font_size, FLT_MAX, 0.0f, watermark);
-	float posX = screenSize.x - textSize.x - margin;
-	float posY = screenSize.y - textSize.y - margin;
-	ImGui::GetWindowDrawList()->AddText(Menu::Font, font_size, ImVec2(posX + 1, posY), ImColor(255,255,255), watermark);
-	ImGui::GetWindowDrawList()->AddText(Menu::Font, font_size, ImVec2(posX - 1, posY), ImColor(255, 255, 255), watermark);
-	ImGui::GetWindowDrawList()->AddText(Menu::Font, font_size, ImVec2(posX, posY + 1), ImColor(255, 255, 255), watermark);
-	ImGui::GetWindowDrawList()->AddText(Menu::Font, font_size, ImVec2(posX, posY - 1), ImColor(255, 255, 255), watermark);
+        float margin = 10.f;
+        float paddingX = 10.f;
+        float paddingY = 5.f;
+        float font_size = 24;
 
-	ImGui::GetWindowDrawList()->AddText(Menu::Font, font_size, ImVec2(posX, posY), ImColor(0, 0, 0), watermark);
+        ImVec2 screenSize = ImGui::GetWindowSize();
+        ImVec2 textSize = Menu::Font->CalcTextSizeA(font_size, FLT_MAX, 0.0f, watermark);
+
+        ImVec2 boxMin(screenSize.x - textSize.x - paddingX * 2 - margin, margin);
+        ImVec2 boxMax(screenSize.x - margin, boxMin.y + textSize.y + paddingY * 2);
+
+        ImColor boxFill(0, 0, 0, 180);
+        ImColor boxOutline(255, 255, 255, 220);
+
+        ImDrawList* drawList = ImGui::GetWindowDrawList();
+        drawList->AddRectFilled(boxMin, boxMax, boxFill, 5.f);
+        drawList->AddRect(boxMin, boxMax, boxOutline, 5.f);
+        drawList->AddText(Menu::Font, font_size,
+                ImVec2(boxMin.x + paddingX, boxMin.y + paddingY),
+                ImColor(255, 255, 255), watermark);
 
 	Esp::RenderUpdate();
 	AimAssist::RenderUpdate();
